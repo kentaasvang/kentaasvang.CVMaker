79cbd1c3aa9ba1968b5f92a3b2414c6c4a4ab11c
Kent Martin Åsvang <kent@asvang.no>
2023-09-16 13:23:45 +0200

    removed application-project

--- src/LagDinCv.Application/Interfaces/IFileWriter.cs
+++
@@ -1,6 +0,0 @@
-namespace LagDinCv.Application.Interfaces;
-
-public interface IFileWriter
-{
-    Task<string> WriteContentToFileAsync(string content, string filePath);
-}
--- src/LagDinCv.Application/Interfaces/IPdfBuilder.cs
+++
@@ -1,8 +0,0 @@
-using LagDinCv.Domain.Requests;
-
-namespace LagDinCv.Application.Interfaces;
-
-public interface IPdfBuilder
-{
-    Task<Uri> CreateResume(CreateCvRequest model);
-}
