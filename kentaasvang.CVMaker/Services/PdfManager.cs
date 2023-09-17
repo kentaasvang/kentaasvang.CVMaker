fc28811cfe4541578b8b4bf490cb1646239aea5e
Kent Martin Åsvang <kent@asvang.no>
2023-09-17 13:59:20 +0200

    removed temporary files

--- kentaasvang.CVMaker/Services/PdfManager.cs
+++ kentaasvang.CVMaker/Services/PdfManager.cs
@@ -1,20 +1,57 @@
+using kentaasvang.CVMaker.Forms;
 using kentaasvang.CVMaker.Requests;
 using kentaasvang.PdfLatex;
+using RazorLight;
 
 namespace kentaasvang.CVMaker.Services;
 
-public class PdfBuilder
+public class PdfManager
 {
     private readonly FileWriter _fileWriter;
     private readonly PdfLatexOptions _options;
 
-    public PdfBuilder(FileWriter fileWriter, IConfiguration configuration)
+    public PdfManager(FileWriter fileWriter, IConfiguration configuration)
     {
         _fileWriter = fileWriter;
         _options = configuration.GetSection(PdfLatexOptions.PdfLatex).Get<PdfLatexOptions>()
                    ?? throw new NullReferenceException("PdfLatexOptions can't be null");
     }
 
+    public async Task<string> CreateAsync(CvForm form)
+    {
+        var engine = new RazorLightEngineBuilder()
+            .UseEmbeddedResourcesProject(typeof(CvForm))
+            .SetOperatingAssembly(typeof(CvForm).Assembly)
+            .Build();
+        
+        var templatePath = Path.Combine(_options.TemplateDir, form.TwentySecond);
+        var template = await File.ReadAllTextAsync(templatePath);
+        
+        var tempTemplateName = GetRandomFileNameWithExtension(FileExtension.Tex);
+        
+        var dirPath = Path.Combine(_options.TempFilesDir, tempTemplateName);
+        
+        var result = await engine.CompileRenderStringAsync("key", template, form);
+
+        var pdfLatexBuilder = new PdfLatexBuilder();
+        
+        var filePath = await _fileWriter.WriteContentToFileAsync(result, dirPath);
+        
+        pdfLatexBuilder
+            .OutputDirectory(_options.OutputDir)
+            .File(filePath)
+            .EnableInstaller()
+            .IncludeDirectory(_options.TemplateDir)
+            .NonStopMode();
+        
+        await pdfLatexBuilder.Run();
+        
+        var urlPath = new Uri($"/{Path.GetFileNameWithoutExtension(tempTemplateName)}.pdf", UriKind.Relative);
+        
+        return urlPath.ToString().Substring(1);
+    }
+
+    [Obsolete("Use CreateAsync instead")]
     public async Task<Uri> CreateResume(CreateCvRequest model)
     {
         var templateFileName = model.CvTemplateType.ToFileName();
